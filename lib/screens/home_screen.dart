4c5e96caac6830826e83fd5c6332d3b7571fdbb6
Adlet <ksss90411@gmail.com>
2025-11-03 08:39:29 +0500

    [feat]: made appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,33 +8,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("You pressed the button this many times:"),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          titleSpacing: 0,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(2),
+            child: Container(color: Colors.grey[350], height: 2),
+          ),
+          title: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 24,
+                  backgroundImage: NetworkImage(
+                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F036%2F280%2F651%2Fnon_2x%2Fdefault-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-illustration-vector.jpg&f=1&nofb=1&ipt=e9e2f390b3d1adf1462c5d54d17324325c345d7a126001f9426826615d5c3942',
+                  ),
+                ),
+                const SizedBox(width: 12),
+                const Text(
+                  "Username",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
+          ),
+          actions: const [
+            Icon(Icons.search, color: Colors.black),
+            SizedBox(width: 20),
+            Icon(Icons.more_horiz, color: Colors.black),
+            SizedBox(width: 20),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
       ),
     );
   }
